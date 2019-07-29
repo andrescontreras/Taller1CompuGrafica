badaf7519209ecac12a5260c5534e6db3ce8d201
Andres Contreras <andres_c8@outlook.com>
2019-07-28 23:34:04 -0500

    contorno estrella

--- OpenGL/Punto.hxx
+++ OpenGL/Punto.hxx
@@ -7,7 +7,7 @@ Punto::Punto(float x, float y, float r, float g, float b, float t)
 	color[0] = (1.0f / 255) *r;
 	color[1] = (1.0f / 255) *g;
 	color[2] = (1.0f / 255) *b;
-	posicion[0] = x*20;
+	posicion[0] = x * 20;
 	posicion[1] = y * 20;
 }
 Punto::Punto(float x, float y, float t)
